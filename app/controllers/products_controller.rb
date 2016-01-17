689f12492d3a990251a066776c695425bc2fdf7a
nealdeters <nealdeters@gmail.com>
2016-01-17 16:56:57 -0600

    updated

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,9 +1,40 @@
 class ProductsController < ApplicationController
-  def index
+  
+  def home
     @product = Product.first
   end
 
-  def products
+  def index
     @products = Product.all
   end
+
+  def new
+    
+  end
+
+  def create
+    @product = Product.create({name: params[:name], image: params[:image], description: params[:description], price: params[:price]})
+  end
+
+  def show
+    #@product = Product.find_by(id: params[:id])
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product = Product.find(params[:id])
+
+    @product.update({name: params[:name], image: params[:image], description: params[:description], price: params[:price]})
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+
+    @product.destroy
+  end
+
 end
